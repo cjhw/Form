b4b4086cf7bfb2a223a6f4ee9e4fb3fde011256a
cjhw <1784581164@qq.com>
2023-01-25 22:13:53 +0800

    refactor: 重构Field组件为函数组件

--- src/components/my-rc-field-form/Field.jsx
+++ src/components/my-rc-field-form/Field.jsx
@@ -1,25 +1,67 @@
-import React, { Component } from "react";
+import React, {
+  Component,
+  useLayoutEffect,
+  useReducer,
+  useContext,
+} from "react";
 import FieldContext from "./FieldContext";
 
-export default class Field extends Component {
-  static contextType = FieldContext;
+// export default class Field extends Component {
+//   static contextType = FieldContext;
 
-  componentDidMount() {
-    this.unregister = this.context.registerFieldEntities(this);
-  }
+//   componentDidMount() {
+//     this.unregister = this.context.registerFieldEntities(this);
+//   }
 
-  componentWillUnmount() {
-    this.unregister();
-  }
+//   componentWillUnmount() {
+//     this.unregister();
+//   }
 
-  onStoreChange = () => {
-    this.forceUpdate();
-  };
+//   onStoreChange = () => {
+//     this.forceUpdate();
+//   };
 
-  getControlled = () => {
-    const { getFieldValue, setFieldsValue } = this.context;
-    const { name } = this.props;
+//   getControlled = () => {
+//     const { getFieldValue, setFieldsValue } = this.context;
+//     const { name } = this.props;
 
+//     return {
+//       value: getFieldValue(name),
+//       onChange: (e) => {
+//         const newValue = e.target.value;
+//         setFieldsValue({ [name]: newValue });
+//         console.log(newValue);
+//       },
+//     };
+//   };
+
+//   render() {
+//     const { children } = this.props;
+
+//     const returnChildNode = React.cloneElement(children, this.getControlled());
+
+//     return returnChildNode;
+//   }
+// }
+
+export default function Field(props) {
+  const { children, name } = props;
+
+  const { getFieldValue, setFieldsValue, registerFieldEntities } =
+    useContext(FieldContext);
+
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  useLayoutEffect(() => {
+    const unregister = registerFieldEntities({
+      props,
+      onStoreChange: forceUpdate,
+    });
+
+    return unregister;
+  }, []);
+
+  const getControlled = () => {
     return {
       value: getFieldValue(name),
       onChange: (e) => {
@@ -30,11 +72,6 @@ export default class Field extends Component {
     };
   };
 
-  render() {
-    const { children } = this.props;
-
-    const returnChildNode = React.cloneElement(children, this.getControlled());
-
-    return returnChildNode;
-  }
+  const returnChildNode = React.cloneElement(children, getControlled());
+  return returnChildNode;
 }
